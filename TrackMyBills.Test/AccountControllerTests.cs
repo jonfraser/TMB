42e94fba85775348fa169d1a2e18973f18ed944f
jonfraser <fraser.jc@gmail.com>
2014-10-23 21:44:11 +1000

    add all the original files

---
+++ TrackMyBills.Test/AccountControllerTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TrackMyBills.Controllers;
+using Moq;
+using TrackMyBills.Services;
+using System.Web.Mvc;
+
+namespace TrackMyBills.Test
+{
+    [TestFixture]
+    public class AccountControllerTests
+    {
+        [Test]
+        public static void ShouldReturnDashboardViewWhenLoggedInWithValidUserKey()
+        {
+            //arrange
+            var validUserKey = "valid";
+
+            var mockAccountService = new Mock<IAccountService>();
+            mockAccountService.Setup(m=> m.Login(validUserKey)).Returns(true);
+
+            var ac = new AccountController(mockAccountService.Object);
+                        
+            //act
+            var result = ac.Login(validUserKey,"") as RedirectToRouteResult;
+
+            //assert
+            Assert.AreEqual("Dashboard", result.RouteValues["action"]);
+            Assert.AreEqual("Dashboard", result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public static void ShouldReturnLoginViewWhenLoggedInWithInvalidUserKey()
+        {
+            //arrange
+            var invalidUserKey = "invalid";
+
+            var mockAccountService = new Mock<IAccountService>();
+            mockAccountService.Setup(m=> m.Login(invalidUserKey)).Returns(false);
+
+            var ac = new AccountController(mockAccountService.Object);
+                        
+            //act
+            var result = ac.Login(invalidUserKey,"") as ViewResult;
+
+            //assert
+            Assert.AreEqual("Login", result.ViewName);
+        }
+
+        [Test]
+        public static void ShouldReturnDashboardViewWhenLoggedInWithValidUsernameAndPassword()
+        {
+            //arrange
+            var validUsername = "valid";
+            var validPassword = "valid";
+            var validUserKey = "valid";
+
+            var mockAccountService = new Mock<IAccountService>();
+            mockAccountService.Setup(m => m.Login(validUserKey)).Returns(true);
+            mockAccountService.Setup(m => m.ComputeUserKey(validUsername, validPassword)).Returns(validUserKey);
+
+            var ac = new AccountController(mockAccountService.Object);
+
+            //act
+            var result = ac.Login(validUsername,validPassword) as RedirectToRouteResult;
+
+            //assert
+            Assert.AreEqual("Dashboard", result.RouteValues["action"]);
+            Assert.AreEqual("Dashboard", result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public static void ShouldReturnLoginViewWhenLoggedInWithInvalidUsernameAndPassword()
+        {
+            //arrange
+            var invalidUsername = "invalid";
+            var invalidPassword = "invalid";
+            var invalidUserKey = "invalid";
+
+            var mockAccountService = new Mock<IAccountService>();
+            mockAccountService.Setup(m => m.Login(invalidUserKey)).Returns(false);
+            mockAccountService.Setup(m => m.ComputeUserKey(invalidUsername, invalidPassword)).Returns(invalidUserKey);
+
+            var ac = new AccountController(mockAccountService.Object);
+
+            //act
+            var result = ac.Login(invalidUsername, invalidPassword) as ViewResult;
+
+            //assert
+            Assert.AreEqual("Login", result.ViewName);
+
+        }
+
+    }
+}
---
+++ TrackMyBills.Test/DashboardControllerTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TrackMyBills.Controllers;
+using System.Web.Mvc;
+using TrackMyBills.Models;
+using System.Web.Routing;
+using System.Web;
+using Moq;
+using TrackMyBills.Services;
+
+namespace TrackMyBills.Test
+{
+    [TestFixture]
+    public class DashboardControllerTests
+    {
+        #region Dashboard
+
+        [Test]
+        public static void ShouldReturnLoginViewFromDashboardViewWhenNoLoggedInUserCredentialInSession()
+        {
+            //arrange
+            var dc = new DashboardController(new Mock<IBudgetService>().Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+            
+            //act
+            var result = dc.Dashboard();
+            
+            //var result = resultOrig as RedirectToRouteResult;
+
+            //assert
+            Assert.IsNotNull(result);
+            //Assert.AreEqual("Login", result.RouteValues["action"]);
+        }
+
+        [Test]
+        public static void ShouldReturnDashboardViewFromDashboardViewWhenLoggedInUserCredentialInSession()
+        {
+            //arrange
+            var dc = new DashboardController(new Mock<IBudgetService>().Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+
+            var mockContext = new Mock<HttpContextBase>();
+            var mockSession = new Mock<HttpSessionStateBase>();
+            mockContext.Setup(m => m.Session).Returns(mockSession.Object);
+
+
+            var controllerContext = new ControllerContext(mockContext.Object, new RouteData(), dc);
+            dc.ControllerContext = controllerContext;
+
+            dc.HttpContext.Session["LoggedInUser"] = new UserSecurityModel() { UserKey = "valid" };
+
+            //act
+            var result = dc.Dashboard() as ViewResult;
+
+            //assert
+            Assert.AreEqual("Dashboard", result.ViewName);
+        }
+
+        #endregion
+
+        #region Budget
+        
+        [Test]
+        public static void ShouldReturnJsonWithNewBudgetItemGuidWhenSavingPayDetails()
+        {
+            //Arrange
+            var mockBudgetSvc = new Mock<IBudgetService>();
+            var expectedGuid = Guid.NewGuid();
+            var mockBudget = new BudgetModel();
+            mockBudgetSvc.Setup(m => m.Save(mockBudget)).Returns(expectedGuid);
+            var dc = new DashboardController(mockBudgetSvc.Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+
+            //Act
+            var result = dc.SaveBudget(mockBudget) as JsonResult;
+
+            //Assert
+            Assert.AreEqual(expectedGuid, result.Data);
+        }
+
+        [Test]
+        public static void ShouldReturnNewRecordAsJsonWhenAddingNewBudgetItem()
+        {
+
+        }
+
+        [Test]
+        public static void ShouldReturnEditedRecordAsJsonWhenSavingExistingBudgetItem()
+        {
+
+        }
+
+        [Test]
+        public static void ShouldReturnJsonTrueWhenDeletingBudgetItem()
+        {
+
+        }
+
+        [Test]
+        public static void ShouldReturnBudgetViewWhenViewingBudget()
+        {
+            //arrange
+            var dc = new DashboardController(new Mock<IBudgetService>().Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+
+            //act
+            var result = dc.Budget() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Budget", result.ViewName);
+        }
+
+        [Test]
+        public static void BudgetViewShouldContainBudgetModel()
+        {
+            //arrange
+            var mockBudgetService = new Mock<IBudgetService>();
+            var fakeBudgetList = new List<BudgetModel>();
+            mockBudgetService.Setup(m => m.GetBudgetByUserKey("valid")).Returns(fakeBudgetList);
+            var dc = new DashboardController(mockBudgetService.Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+
+            //act
+            var result = dc.Budget() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result.Model);
+            var modelAsTyped = result.Model as List<BudgetModel>;
+            Assert.IsNotNull(modelAsTyped);
+        }
+
+        #endregion
+
+        #region Bill
+
+        //[Test]
+        //public static void ShouldReturnJsonWithNewBillItemGuidWhenSavingBill()
+        //{
+        //    //Arrange
+        //    var mockBillSvc = new Mock<IBillService>();
+        //    var expectedGuid = Guid.NewGuid();
+        //    var mockBill = new BillSaveModel();
+        //    mockBillSvc.Setup(m => m.Save(mockBill)).Returns(expectedGuid);
+        //    var dc = new DashboardController(new Mock<IBudgetService>().Object, mockBillSvc.Object);
+
+        //    //Act
+        //    var result = dc.SaveBill(mockBill);
+
+        //    //Assert
+        //    Assert.AreEqual(expectedGuid, result.Data);
+        //}
+
+        [Test]
+        public static void ShouldReturnBillViewWhenViewingBills()
+        {
+            //arrange
+            var dc = new DashboardController(new Mock<IBudgetService>().Object, new Mock<IBillService>().Object, new Mock<IAuditService>().Object);
+
+            //act
+            var result = dc.Bill() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Bill", result.ViewName);
+        }
+
+        [Test]
+        public static void BillViewShouldContainBillModel()
+        {
+            //arrange
+            var mockBillService = new Mock<IBillService>();
+            var fakeBillList = new List<BillModel>();
+            mockBillService.Setup(m => m.GetCurrentBillsByUserKey("valid")).Returns(fakeBillList);
+            var dc = new DashboardController(new Mock<IBudgetService>().Object, mockBillService.Object, new Mock<IAuditService>().Object);
+
+            //act
+            var result = dc.Bill() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result.Model);
+            var modelAsTyped = result.Model as List<BillModel>;
+            Assert.IsNotNull(modelAsTyped);
+        }
+
+        #endregion
+
+    }
+}
